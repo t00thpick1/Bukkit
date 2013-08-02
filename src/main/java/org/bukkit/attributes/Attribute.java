7c04ddfa830384093010292212735a20d791126f
T00thpick1 <t00thpick1dirko@gmail.com>
2013-08-02 15:34:46 -0400

    Document it

--- src/main/java/org/bukkit/attributes/Attribute.java
+++ src/main/java/org/bukkit/attributes/Attribute.java
@@ -1,12 +1,34 @@
 package org.bukkit.attributes;
 
+/**
+ * Represents an attribute on an entity
+ */
 public interface Attribute {
 
+    /**
+     * Gets the identifier of the attribute type
+     * @return the attribute identifier
+     */
     public AttributeIdentifier getIdentifier();
 
+    /**
+     * Gets the base value of the attribute before modifiers are applied
+     * 
+     * @return the base value of the attribute
+     */
     public double getBaseValue();
 
+    /**
+     * Sets the base value of an Attribute
+     * 
+     * @param value the value to set the base to
+     */
     public void setBaseValue(double value);
 
+    /**
+     * Gets the value of the attribute after modifiers are applied
+     * 
+     * @return the value
+     */
     public double getValue();
 }
