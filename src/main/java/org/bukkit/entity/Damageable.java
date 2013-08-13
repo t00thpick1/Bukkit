447d07f71f76227197832b4d28ef1acd24d7ba04
T00thpick1 <t00thpick1dirko@gmail.com>
2013-08-12 22:25:07 -0400

    Deprecate old max health functions

--- src/main/java/org/bukkit/entity/Damageable.java
+++ src/main/java/org/bukkit/entity/Damageable.java
@@ -70,7 +70,9 @@ public interface Damageable extends Entity {
      * Gets the maximum health this entity has.
      *
      * @return Maximum health
+     * @deprecated use Attributes instead
      */
+    @Deprecated
     double getMaxHealth();
 
     /**
@@ -89,7 +91,9 @@ public interface Damageable extends Entity {
      * Note: An entity with a health bar ({@link Player}, {@link EnderDragon}, {@link Wither}, etc...} will have their bar scaled accordingly.
      *
      * @param health amount of health to set the maximum to
+     * @deprecated use Attributes instead
      */
+    @Deprecated
     void setMaxHealth(double health);
 
     /**
@@ -102,6 +106,8 @@ public interface Damageable extends Entity {
 
     /**
      * Resets the max health to the original amount.
+     * @deprecated does not actually function
      */
+    @Deprecated
     void resetMaxHealth();
 }
