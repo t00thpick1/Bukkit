0dfcbd9abe60ed0380d280826f9bcc3ca31ca216
Likaos <romain.rosique@gmail.com>
2013-12-08 13:52:45 +0100

    [B+C] Add methods to get and set knockback strength in Arrow. Adds BUKKIT-5103

--- src/main/java/org/bukkit/entity/Arrow.java
+++ src/main/java/org/bukkit/entity/Arrow.java
@@ -3,4 +3,20 @@ package org.bukkit.entity;
 /**
  * Represents an arrow.
  */
-public interface Arrow extends Projectile {}
+public interface Arrow extends Projectile {
+
+    /**
+     * Get the knockbackStrength for an arrow. Which is the {@link org.bukkit.enchantments.Enchantment#KNOCKBACK KnockBack} level of
+     * the bow that shot it.
+     * 
+     * @return the knockbackStrength value.
+     */
+    public int getKnockbackStrength();
+
+    /**
+     * Set the knockbackStrength for an arrow.
+     * 
+     * @param knockbackStrength the knockbackStrength value.
+     */
+    public void setKnockbackStrength(int knockbackStrength);
+}
