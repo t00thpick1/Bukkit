80af2955d6f0e61c0be90aeec77a2eb078d3f248
MorphanOne <morphanone@gmail.com>
2013-12-09 03:32:33 -0500

    [B+C] Add setCritical and isCritical methods to Arrow.java, Adds BUKKIT-5113

--- src/main/java/org/bukkit/entity/Arrow.java
+++ src/main/java/org/bukkit/entity/Arrow.java
@@ -19,4 +19,18 @@ public interface Arrow extends Projectile {
      * @param knockbackStrength the knockbackStrength value.
      */
     public void setKnockbackStrength(int knockbackStrength);
+
+    /**
+     * Determines if this projectile is critical.
+     *
+     * @return true if it is critical.
+     */
+    public boolean isCritical();
+
+    /**
+     * Set whether or not this arrow should be critical.
+     *
+     * @param critical whether or not it should be critical.
+     */
+    public void setCritical(boolean critical);
 }
